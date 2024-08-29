1b0e374e77b92ad6beb35d53e092b439318927e3
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-08-29 12:26:38 +0900

    cdk init --language typescript

---
+++ test/hello-aws-cdkv2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HelloAwsCdkv2 from '../lib/hello-aws-cdkv2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-aws-cdkv2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HelloAwsCdkv2.HelloAwsCdkv2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
