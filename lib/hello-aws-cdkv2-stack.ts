02bc03c4f5bf3b0e28f5f85251aba09268378e60
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-08-31 16:35:27 +0900

    cfn のidをbacket name のsuffixに設定するようにした

--- lib/hello-aws-cdkv2-stack.ts
+++ lib/hello-aws-cdkv2-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_s3, Fn } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloAwsCdkv2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const suffix = this.initializeSiffix();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloAwsCdkv2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new aws_s3.Bucket(this, 'TsBucket', {
+      bucketName: `cool-bucket-${suffix}`,
+      lifecycleRules: [
+        {
+          expiration: cdk.Duration.days(3)
+        }
+      ]
+    });
+
+    console.log('bucket name: ' + bucket.bucketName)
+  }
+
+  // Fn は AWS CloudFormation の組み込み関数を提供するクラスのこと。
+  // 1. this.stackId を '/' で分割し、その結果のリストの3番目の要素（インデックス2）を取得。これが shortStackId のこと。
+  // 2. shortStackId を '-' で分割し、その結果のリストの5番目の要素（インデックス4）を取得。これが suffix のこと
+  // これをsuffix にすることでどのcloud formation から実行されたのかがわかる。
+  private initializeSiffix(){
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+    const suffix = Fn.select(4, Fn.split('-', shortStackId))
+
+    return suffix;
   }
 }
